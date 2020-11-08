a931bb2a6f6fb3c06cc3ed857c15574c32d8ef4d
Bejjoeqq <aldhiya.rozak@gmail.com>
2020-11-08 13:15:31 +0700

    r

--- arrowui.py
+++ arrowui.py
@@ -1,17 +1,51 @@
 import msvcrt,os
-def cls():
-    if os.name=="nt":
-        os.system("cls")
-    else:
-        os.system("clear")
-def arrow(text,char="»"):
+def cls(title):
+	if os.name=="nt":
+		os.system("cls")
+	else:
+		os.system("clear")
+	if title is not None:
+		print(title)
+def arrow(*text,title=None,desc=None,index=False,char="»",loc="l",align=True):
+	if len(text)==1 and type(text[0])!="str":
+		text=list(text[0])
+	if index:
+		for y,x in enumerate(text):
+			text[y] = f"{y+1}. {x}"
+	if align:
+		maxi = len(text[0])
+		for x in text:
+			if len(x)>maxi:
+				maxi=len(x)
+		for y,x in enumerate(text):
+			text[y] = f"{' '*(len(char)+1)}{x}"
+			text[y] = f"{text[y]}{' '*(maxi-len(x))}"
 	nn=0
-	cls()
+	cls(title)
 	for y,x in enumerate(text):
 		if y==nn:
-			print(char+" "+x)
+			if loc.lower()=="lr" or loc.lower()=="rl":
+				if align:
+					print(f"{char} {x[len(char)+1:]} {char}")
+				else:
+					print(f"{char} {x} {char}")
+			elif loc.lower()=="r":
+				if align:
+					print(f"{x[len(char)+1:]} {char}")
+				else:
+					print(f"{x} {char}")
+			else:
+				if align:
+					print(f"{char} {x[len(char)+1:]}")
+				else:
+					print(f"{char} {x}")
 		else:
-			print(x)
+			if loc.lower()=="r" and align:
+				print(x[len(char)+1:])
+			else:
+				print(x)
+	if desc is not None:
+		print(desc)
 	while True:
 		if msvcrt.kbhit():
 			n = ord(msvcrt.getch())
@@ -20,27 +54,66 @@ def arrow(text,char="»"):
 				if n==72:
 					if nn>0:
 						nn-=1
-					cls()
+					cls(title)
 					for y,x in enumerate(text):
 						if y==nn:
-							print(char+" "+x)
+							if loc.lower()=="lr" or loc.lower()=="rl":
+								if align:
+									print(f"{char} {x[len(char)+1:]} {char}")
+								else:
+									print(f"{char} {x} {char}")
+							elif loc.lower()=="r":
+								if align:
+									print(f"{x[len(char)+1:]} {char}")
+								else:
+									print(f"{x} {char}")
+							else:
+								if align:
+									print(f"{char} {x[len(char)+1:]}")
+								else:
+									print(f"{char} {x}")
 						else:
-							print(x)
+							if loc.lower()=="r" and align:
+								print(x[len(char)+1:])
+							else:
+								print(x)
+					if desc is not None:
+						print(desc)
 				elif n==80:
 					if nn<len(text)-1:
 						nn+=1
-					cls()
+					cls(title)
 					for y,x in enumerate(text):
 						if y==nn:
-							print(char+" "+x)
+							if loc.lower()=="lr" or loc.lower()=="rl":
+								if align:
+									print(f"{char} {x[len(char)+1:]} {char}")
+								else:
+									print(f"{char} {x} {char}")
+							elif loc.lower()=="r":
+								if align:
+									print(f"{x[len(char)+1:]} {char}")
+								else:
+									print(f"{x} {char}")
+							else:
+								if align:
+									print(f"{char} {x[len(char)+1:]}")
+								else:
+									print(f"{char} {x}")
 						else:
-							print(x)
+							if loc.lower()=="r" and align:
+								print(x[len(char)+1:])
+							else:
+								print(x)
+					if desc is not None:
+						print(desc)
 			elif n==13:
 				break
 	return nn
 def main():
 	txt = ["1. Menu","2. Info","3. Exit","4. Asw","5. Let"]
-	hasil = arrow(txt)
+	txt1 = "asd","sad","asem","askduaueb"
+	hasil = arrow(txt1,title="Welcome",desc="Note : use arrow",char="<->",index=True,loc="rl",align=False)
 	print(hasil)
 if __name__ == '__main__':
 	main()
